dbfb0d2bc3b395b2d01ab2d8fd07fbcc6df70a54
Jonah Graham <jonah@kichwacoders.com>
2019-04-22 13:35:40 -0400

    Promote files from cdt-infra to download.eclipse.org

---
+++ scripts/promote-files-to-download.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -u # run with unset flag error so that missing parameters cause build failure
+set -e # error out on any failed commands
+set -x # echo all commands used for debugging purposes
+
+SSH="ssh -o StrictHostKeyChecking=no genie.cdt@projects-storage.eclipse.org"
+SCP="scp -o StrictHostKeyChecking=no"
+SSHUSER="genie.cdt@projects-storage.eclipse.org"
+DOWNLOAD=/home/data/httpd/download.eclipse.org/tools/cdt
+
+ECHO=echo
+if [ "$DRY_RUN" == "false" ]; then
+   ECHO=""
+else
+    echo Dry run of build:
+fi
+
+# Rather than putting in infra/coding to make this a fully generic script with parameters,
+# just list out what needs to be copied where
+$ECHO $SCP ./download/releases/9.7/* "${SSHUSER}:${DOWNLOAD}/releases/9.7/"
