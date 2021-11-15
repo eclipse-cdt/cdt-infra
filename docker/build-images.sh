585d2514ff2457789f1cb26675babd05a66a3d0a
Jonah Graham <jonah@kichwacoders.com>
2021-11-15 13:19:52 -0500

    Bug 576533: Update to newer Xcode so that mac on Arm can be targetted

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -2,10 +2,10 @@
 
 set -eux
 
-# Xcode_9.4.1.xip is needed to build the OSx cross compiler image from https://download.developer.apple.com/Developer_Tools/Xcode_9.4.1/Xcode_9.4.1.xip
+# Xcode_13.1.xip is needed to build the OSx cross compiler image from https://developer.apple.com/download/all/?q=xcode
 # As an optimization for the rebuild times, the image is only built if the xip is present.
-if [ -f Xcode_9.4.1.xip ]; then
-    docker build --rm  --build-arg Xcodexip=Xcode_9.4.1.xip -f  cdt-infra-build-macos-sdk/ubuntu-18.04/Dockerfile -t cdt-infra-build-macos-sdk:ubuntu-18.04 .
+if [ -f Xcode_13.1.xip ]; then
+    docker build --rm  --build-arg Xcodexip=Xcode_13.1.xip -f  cdt-infra-build-macos-sdk/ubuntu-18.04/Dockerfile -t cdt-infra-build-macos-sdk:ubuntu-18.04 .
 else
     echo "MacOSX SDK & Toolchain build is being skipped"
 fi 
--- docker/cdt-infra-build-macos-sdk/Readme.md
+++ docker/cdt-infra-build-macos-sdk/Readme.md
@@ -1,3 +1,3 @@
 This directory contains dockerfiles to build a Cross platform MacOS SDK.
 
-A download of [Xcode_9.4.1.xip](https://download.developer.apple.com/Developer_Tools/Xcode_9.4.1/Xcode_9.4.1.xip) (visit [here](https://developer.apple.com/download/more/) first to logon to Apple if needed) is needed to build this image.
+A download of [Xcode_13.1.xip](https://developer.apple.com/download/all/?q=xcode) (visit [here](https://developer.apple.com/download/more/) first to logon to Apple if needed) is needed to build this image.
